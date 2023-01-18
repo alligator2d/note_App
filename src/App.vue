32b472115ababbcb0e27dbdda67614ae36910d41
Oleg <alligator2d@rambler.ru>
2023-01-18 09:52:56 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+	<div class="wrapper">
+		<div class="wrapper-content">
+			<section>
+				<div class="container">
+					<h1>{{title}}</h1>
+					
+					<Message v-if="message" :message="message"></Message>
+					
+					<div class="new-note">
+						<input v-model="note.title" type="text">
+						<textarea v-model="note.descr"></textarea>
+						<button @click="addNote">New note</button>
+					</div>
+					<!--	note list-->
+					<div class="notes">
+						<div class="note" v-for="(note,index) in notes" :key="index">
+							<div class="note-header">
+								<p>{{note.title}}</p>
+							</div>
+							<div class="note-body">
+								<p>{{note.descr}}</p>
+								<span>{{note.date}}</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+		
+		
+	</div>
 </template>
 
 <script>
-
+import Message from "@/components/Message.vue";
+import NewNote from "@/components/NewNote.vue";
 export default {
-  
+	components: {
+		Message, NewNote
+	},
+  data() {
+	  return {
+		  title: "Notes App",
+		  message: null,
+		  note: {
+			  title: "",
+			  descr: ""
+		  },
+		  notes: [
+			  {
+				  title: "First note",
+				  descr: "Description for first note",
+				  date: new Date(Date.now()).toLocaleString()
+			  },
+			  {
+				  title: "Second note",
+				  descr: "Description for second note",
+				  date: new Date(Date.now()).toLocaleString()
+			  },
+			  {
+				  title: "Third note",
+				  descr: "Description for third note",
+				  date: new Date(Date.now()).toLocaleString()
+			  }
+		  ]
+	  }
+  },
+	methods: {
+		addNote() {
+			console.log(this.note);
+			let { title, descr } = this.note;
+			if (title === '') {
+				this.message = 'Error - note is empty =(';
+				return false;
+			}
+			this.notes.push({
+				title,
+				descr,
+				date: new Date(Date.now()).toLocaleString(),
+			});
+			this.note.title='';
+			this.note.descr = '';
+			this.message = null;
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
